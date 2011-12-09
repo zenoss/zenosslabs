fe5c83f54fffdf8a8ef8c25b65a9ed634005f7d1
Chet Luther <cluther@zenoss.com>
2011-12-09 14:18:19 -0600

    Zenoss flavor not part of slave definition.

--- chef/cookbooks/zenosslabs/recipes/buildslave.rb
+++ chef/cookbooks/zenosslabs/recipes/buildslave.rb
@@ -15,6 +15,26 @@ include_recipe "zenosslabs::default"
 include_recipe "java::sun"
 
 
+user "jenkins" do
+    comment "Jenkins Build Slave"
+    home "/var/lib/jenkins"
+end
+
+directory "/var/lib/jenkins/.ssh" do
+  owner "jenkins"
+  group "jenkins"
+  mode "0700"
+  action :create
+end
+
+cookbook_file "/var/lib/jenkins/.ssh/authorized_keys" do
+    source "jenkins.ssh/id_rsa.pub"
+    mode 0600
+    owner "jenkins"
+    group "jenkins"
+end
+
+
 # Install SSH keys for GitHub.
 # cookbook_file "/var/lib/jenkins/.ssh/id_rsa" do
 #     source "jenkins.ssh/id_rsa"
